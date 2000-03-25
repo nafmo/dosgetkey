cd940aa0120f39ec3508649119883fa4cc75bd74
Peter Karlsson <peter@softwolves.pp.se>
2000-03-25 23:30:20 +0000

    Centered prompts. Long dates. Disables Ctrl-Break. Selective keys. Better error indication.

--- getkey.c
+++ getkey.c
@@ -15,8 +15,14 @@
 #include <dos.h>
 #include <ctype.h>
 
+#define KEYS  14
+
+#define TRUE  1
+#define FALSE 0
+typedef int BOOL;
+
 /* Labels to print for verbose mode */
-const char *key[] = {
+const char *key[KEYS + 1] = {
     "Escape", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10",
     "F11", "F12", "Enter", "Timeout"
 };
@@ -35,23 +41,25 @@ typedef struct
 const char *dateformat[] = {
     "%m%%c%d%%c%Y %H%%c%M%%c%S ",
     "%d%%c%m%%c%Y %H%%c%M%%c%S ",
-    "%Y%%c%m%%c%d %H%%c%M%%c%S "
+    "%Y%%c%m%%c%d %H%%c%M%%c%S ",
+    "%a %d %b %Y %H%%c%M%%c%S "
 };
 
 /* Beep at 1KHz for 0.2 seconds */
-void emitbeep(void)
+int emitbeep(void)
 {
     sound(1000);
     delay(200);
     nosound();
+    return 1;
 }
 
 /* Print date and time in local format */
-void printdate(time_t now)
+void printdate(time_t now, BOOL longdate)
 {
     static nls_t    nls;
-    static int      firsttime = 1;
-    char            format[25];
+    static BOOL     firsttime = TRUE;
+    char            format[32];
     struct tm       *tm_p;
 
     /* On first call, retrieve date format (etc) from DOS */
@@ -64,89 +72,151 @@ void printdate(time_t now)
         s.ds   = FP_SEG(&nls);
         r.x.dx = FP_OFF(&nls);
         intdosx(&r, &r, &s);
-        firsttime = 0;
+        firsttime = FALSE;
     }
 
     /* Print a date-time string */
     tm_p = localtime(&now);
-    strftime(format, 25, dateformat[nls.dateformat], tm_p);
-    cprintf(format, nls.datesep[0], nls.datesep[0],
-                    nls.timesep[0], nls.timesep[0]);
+    if (longdate)
+    {
+        strftime(format, sizeof(format), dateformat[3], tm_p);
+        cprintf(format, nls.timesep[0], nls.timesep[0]);
+    }
+    else
+    {
+        strftime(format, sizeof(format), dateformat[longdate], tm_p);
+        cprintf(format, nls.datesep[0], nls.datesep[0],
+                        nls.timesep[0], nls.timesep[0]);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    int         verbose, beep, showdate, c, rc, i, j;
+    int         c, rc, i, j, x;
+    BOOL        disable[14], verbose, beep, showdate, longdate, center;
     char        *prompt;
     time_t      timeout, now, justthen;
 
     /* Help wanted? */
     if (2 == argc && strcmp(argv[1], "/?") == 0)
     {
-        cprintf("Usage: %s [/V] [/B] [/D] [/Pprompt] [t]\r\n\r\n", argv[0]);
-        cprintf("  /V        Verbose; Print the name of the key pressed\r\n");
+        cprintf("Usage: %s [/B] [/C] [/D] [/L] [/Pprompt] [/V] [t] [key]\r\n\r\n", argv[0]);
         cprintf("  /B        Beep; Produce a sound if an illegal key was pressed\r\n");
+        cprintf("  /C        Center; Center prompt on line\r\n");
         cprintf("  /D        Date; Display date and time\r\n");
+        cprintf("  /L        Long date; Display verbose date format\r\n");
         cprintf("  /Pprompt  Prompt; Message to display\r\n");
+        cprintf("  /V        Verbose; Print the name of the key pressed\r\n");
         cprintf("  t         Timeout (numeric); Number of seconds to wait (default: 30)\r\n");
+        cprintf("  key       Name of key; List keys to DISABLE\r\n");
         cprintf("\r\nExit codes: 0 = Escape, 1-12 = F-key, 13 = Enter, 14 = Timeout\r\n");
         return 15;
     }
 
-    /* Check parameters */
-    verbose = 0;
-    beep = 0;
-    showdate = 0;
+    /* Defaults */
+    verbose  = FALSE;
+    beep     = FALSE;
+    showdate = FALSE;
+    longdate = FALSE;
+    center   = FALSE;
+
     timeout = time(NULL) + 30;
     prompt = NULL;
 
+    for (i = 0; i < KEYS; i ++)
+        disable[i] = FALSE;
+
+    /* Check parameters */
     for (i = 1; i < argc; i ++)
     {
         if ('/' == argv[i][0])
         {
-            int stay = 1;
+            BOOL stay = TRUE;
 
             for (j = 1; argv[i][j] && stay; j ++)
             {
                 switch (toupper(argv[i][j]))
                 {
-                     case 'V':
-                         verbose = 1;
+                     case 'B':
+                         beep = TRUE;
                          break;
 
-                     case 'B':
-                         beep = 1;
+                     case 'C':
+                         center = TRUE;
                          break;
 
                      case 'D':
-                         showdate = 1;
+                         showdate = TRUE;
+                         break;
+
+                     case 'L':
+                         longdate = TRUE;
                          break;
 
                      case 'P':
                          prompt = &argv[i][j + 1];
-                         stay = 0;
+                         stay = FALSE;
+                         break;
+
+                     case 'V':
+                         verbose = TRUE;
                          break;
 
                      default:
                          cprintf("Illegal option: %s\r\n", argv[i]);
+                         cprintf("                %*c^\r\n", j, ' ');
                          return 15;
                 }
             }
         }
-        else
+        else if (isdigit(argv[i][0]))
         {
             int seconds;
 
             sscanf(argv[i], "%d", &seconds);
             timeout = time(NULL) + seconds;
         }
+        else
+        {
+            int found = -1;
+
+            for (j = 0; j < KEYS; j ++)
+                if (stricmp(argv[i], key[j]) == 0)
+                    found = j;
+
+            if (-1 == found)
+            {
+                cprintf("Illegal key name: %s\r\n", argv[i]);
+            }
+            else
+            {
+                disable[found] = TRUE;
+            }
+        }
     }
+
+    /* Check if we want the text to be centered */
+    if (center)
+    {
+        /* TODO: Determine screen size */
+        x = 40 - ((showdate ? (longdate ? 25 : 20) : 0) +
+                  (prompt   ? strlen(prompt)       : 0)) / 2;
+
+    }
+    else
+        x = 0;
 
     /* Wait for key */
     rc = -1;
     now = time(NULL);
     justthen = now;
-    if (showdate) printdate(now);
+
+    /* Inhibit Ctrl-Break */
+    ctrlbrk(emitbeep);
+
+    /* Print prompts */
+    if (x) cprintf("%*c", x, ' ');
+    if (showdate) printdate(now, longdate);
     if (prompt) cputs(prompt);
 
     do
@@ -160,7 +230,8 @@ int main(int argc, char *argv[])
             {
                 /* Repaint it on the same line */
                 cputs("\r");
-                printdate(now);
+                if (x) cprintf("%*c", x, ' ');
+                printdate(now, longdate);
                 if (prompt) cputs(prompt);
                 justthen = now;
             }
@@ -197,6 +268,13 @@ int main(int argc, char *argv[])
                 default:
                     if (beep) emitbeep();
                     break;
+            }
+
+            /* Check that we do not press disabled keys */
+            if (rc >= 0 && rc < KEYS && disable[rc])
+            {
+                rc = -1;
+                if (beep) emitbeep();
             }
         }
         now = time(NULL);
